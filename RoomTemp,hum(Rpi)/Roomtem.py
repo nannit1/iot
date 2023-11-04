b12d7f1d67a3c14d97ccef9f2235fbe8ab5f286f
nannit1 <149881781+nannit1@users.noreply.github.com>
2023-11-04 19:56:45 +0530

    Create Roomtem.py

---
+++ RoomTemp,hum(Rpi)/Roomtem.py
@@ -0,0 +1,9 @@
+import Adafruit_DHT
+SENSOR=Adafruit_DHT.DHT11
+PIN=4
+while True:
+  humidity,temperature=Adafruit_DHT.read(SENSOR,PIN)
+  if humidity is not None and temperature is not None:
+    print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature,humidity))
+  else:
+    print('fofjko')
